c4a8bb86957cf1ae61ade70bef1d79cadcb1c1e5
Sudip Sarkar <sudipsarkar419@gmail.com>
2023-01-22 12:29:23 +0530

    first commit

---
+++ controllers/home_controller.js
@@ -0,0 +1,11 @@
+module.exports.Home=function(req,res){
+    return res.render('home',{
+        title:'Home'
+    });
+}
+
+// module.exports.Profile=function(req,res){
+//     res.render('profile',{
+//         title:'ProfilePage'
+//     })
+// }
---
+++ index.js
@@ -0,0 +1,24 @@
+const express=require('express');
+const port=2000;
+const app= express();
+const expressLayouts=require('express-ejs-layouts');
+const db=require('./config/mongoose');
+app.use(expressLayouts);
+
+
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+//static file for assests
+app.use(express.static('./assests'));
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
+//now separate the file
+app.use(express.urlencoded());
+app.use('/',require('./routes'));
+
+app.listen(port,function(err){
+    if(err){console.log(`Error in running the server:${err}`);}
+    console.log(`Yupp! Server running:${port}`);
+});
